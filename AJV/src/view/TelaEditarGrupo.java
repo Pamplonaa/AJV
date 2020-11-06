79162cbebf841ca11666a7ee90571ddf91ee04be
viniciuspizetta <viniciuspzt@gmail.com>
2020-11-06 19:54:27 -0300

    edit group - show name of student to be removed

--- AJV/src/view/TelaEditarGrupo.java
+++ AJV/src/view/TelaEditarGrupo.java
@@ -7,9 +7,11 @@ package view;
 
 import control.ControladorGrupo;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import model.Aluno;
 import model.Grupo;
 
 /**
@@ -38,10 +40,14 @@ public class TelaEditarGrupo extends javax.swing.JFrame {
         jtfTituloGrupo = new javax.swing.JTextField();
         btSalvar = new javax.swing.JButton();
         btCancelar = new javax.swing.JButton();
+        jlAlterarTituloGrupo = new javax.swing.JLabel();
+        jlExcluirAlunoGrupo = new javax.swing.JLabel();
+        jComboBoxExcluirAlunoGrupo = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jlTituloGrupo.setText("Título do Grupo");
+        jlTituloGrupo.setText("Editar Grupo");
+        jlTituloGrupo.setToolTipText("");
 
         btSalvar.setText("Salvar");
         btSalvar.addActionListener(new java.awt.event.ActionListener() {
@@ -57,6 +63,10 @@ public class TelaEditarGrupo extends javax.swing.JFrame {
             }
         });
 
+        jlAlterarTituloGrupo.setText("Alterar Título do Grupo");
+
+        jlExcluirAlunoGrupo.setText("Excluir Aluno do Grupo");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -66,12 +76,16 @@ public class TelaEditarGrupo extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btSalvar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
                         .addComponent(btCancelar))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jlTituloGrupo)
-                            .addComponent(jtfTituloGrupo, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jlAlterarTituloGrupo)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(jtfTituloGrupo, javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jlExcluirAlunoGrupo, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jComboBoxExcluirAlunoGrupo, javax.swing.GroupLayout.Alignment.LEADING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -80,9 +94,15 @@ public class TelaEditarGrupo extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jlTituloGrupo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
+                .addComponent(jlAlterarTituloGrupo)
+                .addGap(16, 16, 16)
                 .addComponent(jtfTituloGrupo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 105, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(jlExcluirAlunoGrupo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jComboBoxExcluirAlunoGrupo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btSalvar)
                     .addComponent(btCancelar))
@@ -148,6 +168,9 @@ public class TelaEditarGrupo extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btCancelar;
     private javax.swing.JButton btSalvar;
+    private javax.swing.JComboBox<String> jComboBoxExcluirAlunoGrupo;
+    private javax.swing.JLabel jlAlterarTituloGrupo;
+    private javax.swing.JLabel jlExcluirAlunoGrupo;
     private javax.swing.JLabel jlTituloGrupo;
     private javax.swing.JTextField jtfTituloGrupo;
     // End of variables declaration//GEN-END:variables
@@ -157,7 +180,20 @@ public class TelaEditarGrupo extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "ID não encontrado :(");
             return false;
         } else {
+            jComboBoxExcluirAlunoGrupo.removeAllItems();
+
             jtfTituloGrupo.setText(grupo.getTitulo());
+
+            ArrayList array = grupo.getAlunos();
+
+            String str = null;
+
+            for (int i = 0; i < array.size(); i++) {
+                Aluno aluno = new Aluno();
+                aluno = (Aluno) array.get(i);
+                jComboBoxExcluirAlunoGrupo.addItem(aluno.getNome());
+            }
+
             return true;
         }
 
